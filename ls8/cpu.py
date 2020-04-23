7eb79e35e28c2f1faa94123152d402f663c0536a
schang1146 <sammychang2185@gmail.com>
2020-04-22 18:05:07 -0700

    finished up to Day 2 task list

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,24 +10,31 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
-        pass
 
-    def load(self):
+
+    def load(self, file=""):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # If no file given, just hardcode a program:
+        if file == "":
+            program = [
+                # From print8.ls8
+                0b10000010, # LDI R0,8
+                0b00000000,
+                0b00001000,
+                0b01000111, # PRN R0
+                0b00000000,
+                0b00000001, # HLT
+            ]
+        # If file given, load file
+        else:
+            program = []
+            f = open(file, "r")
+            for line in f:
+                if line[0] != "#":
+                    program.append(int(line[:8], 2))
 
         for instruction in program:
             self.ram[address] = instruction
@@ -44,12 +51,12 @@ class CPU:
             raise Exception("Unsupported ALU operation")
 
 
-    def ram_read(self):
-        pass
+    def ram_read(self, address):
+        return self.ram[address]
 
 
-    def ram_write(self):
-        pass
+    def ram_write(self, address, value):
+        self.reg[address] = value
 
 
     def trace(self):
@@ -74,4 +81,23 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+
+        HLT = 0b00000001
+        LDI = 0b10000010
+        MUL = 0b10100010
+        PRN = 0b01000111
+
+        while running:
+            if self.ram_read(self.pc) == LDI:
+                self.ram_write(self.ram_read(self.pc + 1), self.ram_read(self.pc + 2))
+                self.pc += 3
+            elif self.ram_read(self.pc) == MUL:
+                self.trace()
+                self.ram_write(self.ram_read(self.pc + 1), (self.reg[self.ram_read(self.pc + 1)] * self.reg[self.ram_read(self.pc + 2)]))
+                self.pc += 3
+            elif self.ram_read(self.pc) == PRN:
+                print(self.reg[self.ram_read(self.pc + 1)])
+                self.pc += 2
+            elif self.ram_read(self.pc) == HLT:
+                running = False
