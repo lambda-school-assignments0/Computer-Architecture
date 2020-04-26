019677e20b87899d7e71a737052d1c3b99a39107
schang1146 <sammychang2185@gmail.com>
2020-04-25 23:27:30 -0700

    push/pop works

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,26 +2,40 @@
 
 import sys
 
-HLT = 0b00000001
-LDI = 0b10000010
-MUL = 0b10100010
-PRN = 0b01000111
+HLT  = 0b00000001
+LDI  = 0b10000010
+MUL  = 0b10100010
+POP  = 0b01000110
+PRN  = 0b01000111
+PUSH = 0b01000101
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.pc = 0
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.running = False
+        # Set up Registers
+        self.registers = {
+            "PC":  0,   # R0 : Program Counter
+            "IR":  0,   # R1 : Instruction Register
+            "MAR": 0,   # R2 : Memory Address Register
+            "MDR": 0,   # R3 : Memory Data Register
+            "FL":  0,   # R4 : Flags
+            "IM":  0,   # R5 : Interrupt Mask
+            "IS":  0,   # R6 : Interrupt Status
+            "SP":  0xF4 # R7 : Stack Pointer
+        }
         # Set up dispatch branch table
         self.dispatch = {
-            HLT: self.hlt,
-            LDI: self.ldi,
-            MUL: self.mul,
-            PRN: self.prn
+            HLT:  self.hlt,
+            LDI:  self.ldi,
+            MUL:  self.mul,
+            POP:  self.pop,
+            PRN:  self.prn,
+            PUSH: self.push
         }
 
 
@@ -50,16 +64,16 @@ class CPU:
                         if line[0] != "#":
                             program.append(int(line.split("#")[0].strip("\n"), 2))
 
-                for instruction in program:
-                    self.ram[address] = instruction
-                    address += 1
-
             except FileNotFoundError:
                 print(f"{sys.argv[0]}: could not find {sys.argv[1]}")
                 sys.exit(2)
         else:
             print("Too many arguments given!")
             sys.exit(2)
+
+        for instruction in program:
+            self.ram[address] = instruction
+            address += 1
 
     ### OPERATIONS ###
     def alu(self, op, reg_a, reg_b):
@@ -75,18 +89,31 @@ class CPU:
 
 
     def ldi(self):
-        self.ram_write(self.ram_read(self.pc + 1), self.ram_read(self.pc + 2))
-        self.pc += 3
+        self.ram_write(self.ram_read(self.registers["PC"] + 1), self.ram_read(self.registers["PC"] + 2))
+        self.registers["PC"] += 3
 
 
     def mul(self):
-        self.alu("MUL", self.ram_read(self.pc + 1), self.ram_read(self.pc + 2))
-        self.pc += 3
+        self.alu("MUL", self.ram_read(self.registers["PC"] + 1), self.ram_read(self.registers["PC"] + 2))
+        self.registers["PC"] += 3
+
+
+    def pop(self):
+        self.reg[self.ram[self.registers["PC"] + 1]] = self.ram[self.registers["SP"]]
+        self.ram[self.registers["SP"]] = 0
+        self.registers["SP"] += 1
+        self.registers["PC"] += 2
 
 
     def prn(self):
-        print(self.reg[self.ram_read(self.pc + 1)])
-        self.pc += 2
+        print(self.reg[self.ram_read(self.registers["PC"] + 1)])
+        self.registers["PC"] += 2
+
+
+    def push(self):
+        self.registers["SP"] -= 1
+        self.ram[self.registers["SP"]] = self.reg[self.ram[self.registers["PC"] + 1]]
+        self.registers["PC"] += 2
 
 
     def hlt(self):
@@ -108,12 +135,12 @@ class CPU:
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
+            self.registers["PC"],
             #self.fl,
             #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.ram_read(self.registers["PC"]),
+            self.ram_read(self.registers["PC"] + 1),
+            self.ram_read(self.registers["PC"] + 2)
         ), end='')
 
         for i in range(8):
@@ -126,4 +153,4 @@ class CPU:
         self.running = True
 
         while self.running:
-            self.dispatch[self.ram[self.pc]]()
+            self.dispatch[self.ram[self.registers["PC"]]]()
